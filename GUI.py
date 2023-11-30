80f8f00fabd474bdfb2a7b2395d5b1016275f475
Huikun Zheng <huikunzheng9955@gmail.com>
2023-11-29 19:43:39 -0800

    ui done

--- GUI.py
+++ GUI.py
@@ -1,14 +1,38 @@
 import tkinter as tk
 from tkinter import filedialog
 
+from fileProcessor import file_process, save_file
+
+formatted_doc = None
+
+
 def import_file():
+    global formatted_doc, status_label
     file_path = filedialog.askopenfilename()
-    print(f"Imported file: {file_path}")
+
+    if file_path:  # Check if a file was selected
+        name = filename_entry.get()
+        status_label.config(text="Processing...")
+        root.update()  # Update the GUI to reflect the new label before processing starts
+        formatted_doc = file_process(file_path, name)
+        status_label.config(text="Import complete. File ready for export.")
+        print(f"Imported file: {file_path}")
+    else:
+        status_label.config(text="No file selected. Please select a file.")
+
 
 def export_file():
     default_name = filename_entry.get() + ".docx"
     file_path = filedialog.asksaveasfilename(defaultextension=".docx", initialfile=default_name)
-    print(f"Exported file: {file_path}")
+
+    if file_path:  # Check if a path was provided
+        global formatted_doc
+        save_file(formatted_doc, file_path)
+        status_label.config(text="Export successful.")
+        print(f"Exported file: {file_path}")
+    else:
+        status_label.config(text="Export cancelled.")
+
 
 # Create the main window
 root = tk.Tk()
@@ -16,7 +40,7 @@ root.title("File Import/Export")
 root.geometry("400x200")  # Adjust the size as needed
 
 # Create a label for status messages
-status_label = tk.Label(root, text="Please upload your file", height=2)
+status_label = tk.Label(root, text="Please enter the title before importing your file", height=2)
 status_label.pack(pady=5)
 
 # Create an Entry widget for the filename
